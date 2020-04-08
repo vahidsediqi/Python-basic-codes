2780c33c6b8f3e11cdc3739a1fb3eac090818062
vahidsediqi <vsediqi@live.com>
2020-04-08 12:31:45 +0300

    variables

---
+++ Variables/variables.py
@@ -0,0 +1,27 @@
+# int
+students_count = 1000
+
+# floating
+rating = 4.99
+
+# bolean
+is_published = False
+
+# string
+course_name = "Phython"
+
+# in string we can use Triple quotes, if we have mutliple lines of string
+course_details = """ This paython course will be published soon
+Author: Vahid Sediqi
+language: English and Dari """
+print(course_details)
+
+# multi line variables in one line
+x, y = 1, 2
+
+print(x + y)
+
+# To know the type of a variable we have a build-in function
+
+print(type(course_details))
+print(type(students_count))
